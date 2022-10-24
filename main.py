8aee793230987624b2dd47194827319903db829b
liquidlyn <lyn10050728@gmail.com>
2022-10-24 22:47:21 +0800

    2022 update

--- main.py
+++ main.py
@@ -9,49 +9,70 @@ loginpwd = input("密碼: ")
 driver = webdriver.Edge("msedgedriver.exe")
 driver.get('https://www.i-sharing.com.tw/card/main')
 
-driver.find_element_by_class_name('menu-toggler.menu-toggler-open.pos-a').click()
-driver.find_element_by_class_name('books-fb-name').click()
-driver.find_element_by_id('email').send_keys(loginid)
-driver.find_element_by_id('pass').send_keys(loginpwd)
-driver.find_element_by_id('loginbutton').click()
+driver.find_element(By.CLASS_NAME,'header__menu__icon.books-menu-open').click()
+driver.find_element(By.CLASS_NAME,'books-fb-name').click()
+driver.find_element(By.ID,'email').send_keys(loginid)
+driver.find_element(By.ID,'pass').send_keys(loginpwd)
+driver.find_element(By.ID,'loginbutton').click()
 time.sleep(2)
-driver.find_element(By.LINK_TEXT, "關閉").click()
 
-for i in range(11):
-    driver.find_element(By.ID, "btn-create-card").click()
+for i in range(10):
+    while True:
+        try:        
+            driver.find_element(By.LINK_TEXT, "立即打造").click()
+            time.sleep(1)
+        except:
+            continue
+        break
+    while True:
+        try:        
+            driver.find_element(By.XPATH,'//*[@id="app"]/div[2]/section[1]/div/picture['+str(i+1)+']/img').click()
+            time.sleep(1)
+        except:
+            continue
+        break
     time.sleep(1)
-    if i !=0:
-        for times in range(i):
-            driver.find_element(By.CSS_SELECTOR, ".box-content > .swiper > .swiper-button-next").click()
-            
-    driver.find_element(By.LINK_TEXT, "下一步").click()
+    driver.find_element(By.CLASS_NAME, "greeting__step2-from").send_keys("name1")
+    driver.find_element(By.CLASS_NAME, "greeting__step2-to").send_keys("name2")
+    driver.find_element(By.CLASS_NAME, "greeting__step2-textarea").send_keys("msg")
+    driver.find_element(By.CLASS_NAME, "greeting__step2-input-upload").send_keys(os.getcwd()+"\\cat-18.jpg")
+    for j in range(2):
+        while True:
+            try:        
+                driver.find_element(By.LINK_TEXT, "下一步").click()
+                time.sleep(1)
+            except:
+                continue
+            break
     time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, ".input-text:nth-child(2)").send_keys("name1")
-    driver.find_element(By.CSS_SELECTOR, ".input-text:nth-child(3)").send_keys("name2")
-    driver.find_element(By.LINK_TEXT, "下一步").click()
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, "#box-step-3 .message").send_keys("content**")
-
-    driver.find_element(By.CSS_SELECTOR, ".input-file").send_keys(os.getcwd()+"\\cat-18.jpg")
-    time.sleep(1)
-    driver.find_element(By.LINK_TEXT, "下一步").click()
-    time.sleep(1)
-    driver.find_element(By.LINK_TEXT, "下一步").click()
-    time.sleep(1)
-    driver.find_element(By.CLASS_NAME, "btn.btn-cta.kerning-2.pink.skip").click()
-    
-    time.sleep(2)
     driver.find_element(By.LINK_TEXT, "完成賀卡").click()
-    time.sleep(1)
+    time.sleep(3)
     
     window_before = driver.window_handles[0]
-    driver.find_element(By.CSS_SELECTOR, ".btn-share-line").click()
+    while True:
+        try:        
+            driver.find_element(By.CLASS_NAME, "greeting__share-btn--line").click()
+            time.sleep(1)
+        except:
+            continue
+        break
     time.sleep(3)
     window_after = driver.window_handles[1]
     driver.switch_to.window(window_after)
     driver.close()
     driver.switch_to.window(window_before)
-    driver.find_element(By.LINK_TEXT, "關閉").click()
-    driver.find_element(By.LINK_TEXT, "再寄一張賀卡").click()
-
-input('Finished sucessfully.')   
+    driver.find_element(By.CLASS_NAME, "books-popup-close").click()
+    while True:
+        try:        
+            driver.find_element(By.LINK_TEXT, "再寄一張賀卡").click()
+            time.sleep(1)
+        except:
+            continue
+        break
+    if i <8:
+        print('No.'+str(i+1)+' card is completed, there are '+str(9-i)+' unfinished cards.')
+    if i==8:
+        print('No.'+str(i+1)+' card is completed, there is 1 unfinished card.')
+    
+    
+input('Finished sucessfully.')
